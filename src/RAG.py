5332c408954200ccd4537ac3c36615e678b5fbb4
Artmiss G <artmiss1380@gmail.com>
2024-08-17 22:07:27 +0330

    Added functionality to load and store indexes

--- src/RAG.py
+++ src/RAG.py
@@ -10,6 +10,9 @@ from llama_index.core import (
     SimpleDirectoryReader,
     VectorStoreIndex,
     get_response_synthesizer,
+    StorageContext,
+    load_index_from_storage,
+
 )
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.retrievers import(
@@ -40,35 +43,30 @@ def load_documents(doc_path: Path) -> list[Document]:
     documents = SimpleDirectoryReader(input_files=[doc_path], file_extractor=file_extractor).load_data()
     return documents
 
-def setup_llm_and_embedding():
-    """
-    Set up LLM and embedding models.
-    """
+def setup_llm():
     llm = Groq(
         model="llama3-groq-70b-8192-tool-use-preview",
         api_key=GROQ_API_KEY
     )
+    Settings.llm = llm 
+    return llm
 
+def setup_embedding():
     embed_model = CohereEmbedding(
         api_key=COHERE_API_KEY,
         model_name="embed-english-v3.0",
         input_type="search_query",
     )
 
-    Settings.llm = llm 
     Settings.embed_model = embed_model
-    # Settings.node_parser = SentenceSplitter(chunk_size=512, chunk_overlap=20)
-    # Settings.num_output = 512
-    # Settings.context_window = 3900
 
-    return embed_model
-
-def create_index_and_query_engine(documents, embed_model, k=3):
-    """
-    Create index and query engine.
-    """
+def create_index(documents, embed_model, save_index=False):
     index = VectorStoreIndex.from_documents(documents, model=embed_model)
+    if save_index:
+        index.storage_context.persist(persist_dir=f"data/.index/{documents[0].metadata["file_name"]}") # data/stored/file_name
+    return index
 
+def create_query_engine(index, k=3):
     retriever = VectorIndexRetriever(
         index=index,
         similarity_top_k=k,
@@ -83,20 +81,36 @@ def create_index_and_query_engine(documents, embed_model, k=3):
 
     return query_engine
 
-def run():
-    # Load and process data
-    doc_path = Path("data/Academic-CV-V1 .pdf")
-    documents = load_documents(doc_path)
+def run(doc_path, rebuild_index=True, index_path=None, save_index=False):
+    embed_model = setup_embedding() 
+    setup_llm() # setting up the global llm
 
-    embed_model = setup_llm_and_embedding() # Setup LLM and embedding
-    query_engine = create_index_and_query_engine(documents, embed_model) # Create index and query engine
+    # loading the index
+    if rebuild_index is False: 
+        print("Loading Index...")
+        if index_path is None:
+            raise ValueError("index_path is required if rebuild_index is False")
+        storage_context = StorageContext.from_defaults(persist_dir=index_path) # rebuild storage context
+        index = load_index_from_storage(storage_context)
+    # rebuilding the index
+    else : 
+        print("Rebuilding Index...")
+        documents = load_documents(doc_path)
+        index = create_index(documents, embed_model, save_index)
 
-    # Get user query and generate response
-    query = input("-> :")
+    query_engine = create_query_engine(index, k=5) #! if the found documents is less than 5 ?
+
+    query = input("-> : ")
     response = query_engine.query(query)
 
     return response
 
 if __name__ == "__main__":
-    response = run()
+    doc_path = Path("data/Academic-CV-V1 .pdf")
+    response = run(
+        doc_path,
+        # save_index=True,
+        rebuild_index=False,
+        index_path=f"data/.index/{doc_path.name}", # index name is the name of the uploaded file
+    )
     pprint(response.response)
